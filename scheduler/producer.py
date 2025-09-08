bd5ebd85d38d82893c93a288ed88826594b2b870
somchoonn <chanokchon.pancome@gmail.com>
2025-09-08 20:06:04 +0700

    Finish Stage3

---
+++ scheduler/producer.py
@@ -0,0 +1,24 @@
+import os
+import pika
+
+
+def produce(host, body):
+    rabbitmq_user = os.getenv("RABBITMQ_DEFAULT_USER")
+    rabbitmq_pass = os.getenv("RABBITMQ_DEFAULT_PASS")
+    credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_pass)
+    connection = pika.BlockingConnection(parameters = pika.ConnectionParameters(host, credentials=credentials))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange="jobs", exchange_type="direct")
+    channel.queue_declare(queue="router_jobs")
+    channel.queue_bind(
+        queue="router_jobs", exchange="jobs", routing_key="check_interfaces"
+    )
+
+    channel.basic_publish(exchange="jobs", routing_key="check_interfaces", body=body)
+
+    connection.close()
+
+
+if __name__ == "__main__":
+    produce("localhost", "192.168.1.44")
